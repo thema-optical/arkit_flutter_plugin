84b30f984355235515ad89ad079af1bf1880c199
Luca De Menego <43495622+lucademenego99@users.noreply.github.com>
2023-01-09 09:58:31 +0100

    Usa configurazione camera UltraWide se disponibile

--- ios/Classes/ConfigurationBuilders/FaceTrackingConfigurationBuilder.swift
+++ ios/Classes/ConfigurationBuilders/FaceTrackingConfigurationBuilder.swift
@@ -4,7 +4,16 @@ import ARKit
 #if !DISABLE_TRUEDEPTH_API
 func createFaceTrackingConfiguration(_ arguments: Dictionary<String, Any>) -> ARFaceTrackingConfiguration? {
     if(ARFaceTrackingConfiguration.isSupported) {
-        return ARFaceTrackingConfiguration()
+        let config = ARFaceTrackingConfiguration()
+        if #available(iOS 14.5, *) {
+            for videoFormat in ARFaceTrackingConfiguration.supportedVideoFormats {
+                if videoFormat.captureDeviceType == .builtInUltraWideCamera {
+                    config.videoFormat = videoFormat
+                    break
+                }
+            }
+        }
+        return config
     }
     return nil
 }
